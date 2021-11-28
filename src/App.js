7b65071fb18fc93b9fa14c247c0eb9e9d0350d2d
Ayush Daharia <dahariaayush@gmail.com>
2021-11-29 01:14:31 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { uuid } from 'uuidv4';
+import api from "./api/contact"
 import './App.css';
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import ContactList from "./components/ContactList";
+import ContactDetail from "./components/ContactDetail";
+
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([])
+
+  //Retrive Contacts
+  const retriveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  };
+
+  const addContactHandler = async (contact) => {
+    console.log(contact);
+    const request = {
+      id: uuid(),
+      ...contact,
+    };
+
+    const response = await api.post("/contacts", request);
+    console.log(response);
+    setContacts([...contacts, response.data]);
+  };
+
+  const updateContactHandler = async (contact) => {
+    const response = await api.put(`/contacts/${contact.id}`, contact);
+    const { id, name, email } = response.data;
+    setContacts(
+      contacts.map(contact => {
+      return contact.id === id ? { ...response.data } : contact;
+    })
+    );
+  };
+
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+
+  };
+
+  const searchHandler = (searchTerm)=> {
+    setSearchTerm(searchTerm);
+    if(searchTerm !== "") {
+      const newContactList = contacts.filter((contact) => {
+        return Object.values(contact)
+        .join("")
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      });
+      setSearchResults(newContactList);
+    }
+    else {
+      setSearchResults(contacts);
+    }
+  };
+
+  useEffect(() => {
+    // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    // if (retriveContacts) setContacts(retriveContacts);
+    const getAllContacts = async () => {
+      const allContacts = await retriveContacts();
+      if (allContacts) setContacts(allContacts);
+    };
+    getAllContacts();
+  }, []);
+
+
+  useEffect(() => {
+    // localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <ContactList
+                {...props}
+                contacts={searchTerm.length < 1 ? contacts : searchResults}
+                getContactId={removeContactHandler}
+                term={searchTerm}
+                searchKeyword={searchHandler}
+              />
+            )}
+          />
+          <Route
+            path="/add"
+            render={(props) => (<AddContact {...props} addContactHandler={addContactHandler}
+            />
+            )}
+          />
+
+          <Route
+            path="/edit"
+            render={(props) => (<EditContact {...props} updateContactHandler={updateContactHandler}
+            />
+            )}
+          />
+
+          <Route path="/contact/:id" component={ContactDetail} />
+
+        </Switch>
+      </Router>
     </div>
   );
 }
